f491aa89731a209a435e30bdd22e85c59dac2733
Diana <kozachenkodiana03@mail.ru>
2020-07-10 22:31:20 +0700

    Advantages v2.0

--- js/main.js
+++ js/main.js
@@ -9,7 +9,10 @@ const extradition = document.querySelector('.extradition'),
   height = document.querySelector('.height'),
   weight = document.querySelector('.weight'),
   size = document.querySelector('.size'),
-  calculationInnerButton = document.querySelector('.calculation__inner-button');
+  calculationInnerButton = document.querySelector('.calculation__inner-button'),
+
+  points = document.querySelectorAll('.point'),
+  lineInnerBox = document.querySelectorAll('.line__inner-box');
 
 const findOption = (select) => {
   for (let i = 0; i < select.length; i++) {
@@ -19,8 +22,20 @@ const findOption = (select) => {
   }
 }
 
+const lightOn = (event) => {
+  const number = event.target.closest('.point').dataset.num - 1;
+
+  lineInnerBox[number].classList.add('box_active');
+}
+
+const lightOff = (event) => {
+  const number = event.target.closest('.point').dataset.num - 1;
+
+  lineInnerBox[number].classList.remove('box_active');
+}
+
 calculationInnerButton.addEventListener('click', () => {
-  let price = 500;  
+  let price = 500;
   const curExtradition = findOption(extradition);
   const curKind = findOption(kind);
   const curCity = findOption(city);
@@ -29,16 +44,23 @@ calculationInnerButton.addEventListener('click', () => {
   const curLength = length.value;
   const curHeight = height.value;
   const curWeight = weight.value;
-  const curSize = size.value;  
+  const curSize = size.value;
 
   if (curWidth > 0 && curLength > 0 && curHeight > 0 && curWeight > 0 && curSize > 0) {
-    price = Math.round(price * curExtradition.dataset.koef * curKind.dataset.koef  * curCity.dataset.koef * curVariant.dataset.koef * curWidth * width.dataset.koef * curLength * length.dataset.koef * curHeight * height.dataset.koef * curWeight * weight.dataset.koef * curSize * size.dataset.koef);
+    price = Math.round(price * curExtradition.dataset.koef * curKind.dataset.koef * curCity.dataset.koef * curVariant.dataset.koef * curWidth * width.dataset.koef * curLength * length.dataset.koef * curHeight * height.dataset.koef * curWeight * weight.dataset.koef * curSize * size.dataset.koef);
 
     alert(price);
-    
+
   } else {
     alert('Вы ввели не все данные или недопустимые значения');
   }
-});  
+});
 
-
+points.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    lightOn(event)
+  });
+  item.addEventListener('mouseout', () => {
+    lightOff(event);
+  });
+})
